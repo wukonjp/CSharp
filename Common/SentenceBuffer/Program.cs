a16e7cc6e2cd21809f37faaebcc705bc7a140bf8
wukonjp <wukon@hotmail.com>
2020-12-06 11:08:29 +0900

    追加

---
+++ Common/SentenceBuffer/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SentenceBuffer
+{
+	/// <summary>
+	/// センテンスを疑似送受信して復元する
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// パケットサイズ
+		/// </summary>
+		const int PacketMax = 7;
+
+		/// <summary>
+		/// エントリーポイント
+		/// </summary>
+		static void Main(string[] args)
+		{
+			// 送信文字列
+			var sendText = "あいうえお\r\n1\r\nかきくけこ\r\n\r\n22\r\nさしすせそ\r\n3";
+
+			// 疑似送信
+			Console.WriteLine("送信：");
+			Console.WriteLine("「{0}」", sendText);
+			var sendBuffer = Encoding.UTF8.GetBytes(sendText);
+
+			// 疑似受信（パケット分割）
+			var packetList = CreatePacketList(sendBuffer);
+			Console.WriteLine("\r\n受信： パケット数{0}", packetList.Count);
+			foreach (var packet in packetList)
+			{
+				Console.WriteLine(packet.Length);
+			}
+
+			// 復元1: NG
+			{
+				Console.WriteLine("\r\n復元1：　パケット文字列化 → 文字列連結");
+				var sb = new StringBuilder();
+				foreach (var packet in packetList)
+				{
+					sb.Append(Encoding.UTF8.GetString(packet));
+				}
+
+				var restoredText = sb.ToString();
+				Console.WriteLine("「{0}」", restoredText);
+			}
+
+			// 復元2: GOOD
+			{
+				Console.WriteLine("\r\n復元2：　パケット連結 → 文字列化");
+				var readBuffer = Join(packetList);
+
+				var restoredText = Encoding.UTF8.GetString(readBuffer);
+				Console.WriteLine("「{0}」", restoredText);
+			}
+
+			// 復元3: GOOD
+			{
+				Console.WriteLine("\r\n復元3：　パケット連結 → CRLF分割 → 文字列化");
+				var readBuffer = Join(packetList);
+
+				byte[] rest;
+				var sentenceList = SplitByCRLF(readBuffer, Encoding.UTF8, out rest);
+				foreach (var sentence in sentenceList)
+				{
+					Console.WriteLine("「{0}」", sentence);
+				}
+
+				if (rest != null)
+				{
+					Console.WriteLine("余りバイト数： {0}", rest.Length);
+				}
+			}
+
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// パケットリストを作成する
+		/// </summary>
+		private static List<byte[]> CreatePacketList(byte[] sendBuffer)
+		{
+			var packetList = new List<byte[]>();
+			for (int i = 0; i < sendBuffer.Length; i += PacketMax)
+			{
+				int blockSize = sendBuffer.Length - i;
+				if (blockSize > PacketMax)
+				{
+					blockSize = PacketMax;
+				}
+
+				var block = new byte[blockSize];
+				Buffer.BlockCopy(sendBuffer, i, block, 0, blockSize);
+				packetList.Add(block);
+			}
+
+			return packetList;
+		}
+
+		/// <summary>
+		/// パケットを連結する
+		/// </summary>
+		private static byte[] Join(List<byte[]> packetList)
+		{
+			int total = 0;
+			foreach (var packet in packetList)
+			{
+				total += packet.Length;
+			}
+
+			var buffer = new byte[total];
+			int index = 0;
+			foreach (var packet in packetList)
+			{
+				Buffer.BlockCopy(packet, 0, buffer, index, packet.Length);
+				index += packet.Length;
+			}
+
+			return buffer;
+		}
+
+		/// <summary>
+		/// バイト配列をCRLFで分割する
+		/// </summary>
+		private static List<string> SplitByCRLF(byte[] buffer, Encoding encoding, out byte[] rest)
+		{
+			rest = null;
+			var list = new List<string>();
+
+			for (int currentIndex = 0; currentIndex < buffer.Length; )
+			{
+				int foundIndex = FindCRLF(buffer, currentIndex);
+				if (foundIndex < 0)
+				{                                                           // CRLF無し
+					int size = buffer.Length - currentIndex;
+					rest = new byte[size];
+					Buffer.BlockCopy(buffer, currentIndex, rest, 0, size);
+					break;
+				}
+				else
+				{															// CRLF有り
+					int size = foundIndex - currentIndex;
+					var text = encoding.GetString(buffer, currentIndex, size);
+					list.Add(text);
+					currentIndex += size + 2;
+				}
+			}
+
+			return list;
+		}
+
+		/// <summary>
+		/// バイト配列内でCRLFを検索してインデックスを返す
+		/// </summary>
+		private static int FindCRLF(byte[] buffer, int startIndex)
+		{
+			unsafe
+			{
+				fixed (Byte* pHead = buffer)
+				{
+					int max = buffer.Length - 1;
+					for (int i = startIndex; i < max; i++)
+					{
+						var p = (UInt16*)(pHead + i);
+						if (*p == 0x0A0D)
+						{													// CRLF有り
+							return i;
+						}
+					}
+
+					return -1;
+				}
+			}
+		}
+	}
+}
---
+++ Common/StringToStruct/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StringToStruct
+{
+	/// <summary>
+	/// 文字列を構造体にバイナリ展開する
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// バイナリデータ構造体
+		/// </summary>
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4, Size = 36)]
+		struct BynaryData
+		{
+			public byte B1;
+			public ushort W1;
+			public uint DW1;
+
+			[MarshalAs(UnmanagedType.LPTStr)]
+			public string LPTText;									// c/c++ char*
+
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 7)]
+			public string ByValText;                                // c/c++ char[SizeConst]
+
+			public byte B2;
+			public ushort W2;
+			public uint DW2;
+		}
+
+		/// <summary>
+		/// エントリーポイント
+		/// </summary>
+		static void Main(string[] args)
+		{
+			var srcData = new BynaryData();
+			srcData.B1 = 0x11;
+			srcData.W1 = 0xFF11;
+			srcData.DW1 = 0xFFFFFF11;
+			srcData.LPTText = "０１２";
+			srcData.ByValText = "３４５";
+			srcData.B2 = 0x22;
+			srcData.W2 = 0xFF22;
+			srcData.DW2 = 0xFFFFFF22;
+			Console.WriteLine("変換前LPT文字列: {0}", srcData.LPTText);
+			Console.WriteLine("変換前ByVal文字列: {0}", srcData.ByValText);
+
+			var buffer1 = StructureToBytes(srcData);
+			Console.WriteLine("変換後の構造体サイズ： {0}", buffer1.Length);
+			foreach (var b in buffer1)
+			{
+				Console.Write("{0:X2} ", b);
+			}
+
+			Console.WriteLine();
+			var dstData = (BynaryData)BytesToStructure(buffer1, typeof(BynaryData));
+			DestroyStructure(buffer1, typeof(BynaryData));
+			Console.WriteLine("変換データ解放：");
+			foreach (var b in buffer1)
+			{
+				Console.Write("{0:X2} ", b);
+			}
+
+			Console.WriteLine("\r\n");
+			Console.WriteLine("復元後LPT文字列: {0}", dstData.LPTText);
+			Console.WriteLine("復元後ByVal文字列: {0}", dstData.ByValText);
+
+			var buffer2 = StructureToBytes(dstData);
+			Console.WriteLine("復元後の構造体サイズ： {0}", buffer2.Length);
+			foreach (var b in buffer2)
+			{
+				Console.Write("{0:X2} ", b);
+			}
+
+			Console.WriteLine();
+			DestroyStructure(buffer2, typeof(BynaryData));
+			Console.WriteLine("復元データ解放：");
+			foreach (var b in buffer2)
+			{
+				Console.Write("{0:X2} ", b);
+			}
+
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// 構造体からバイト配列を生成する
+		/// ※構造体にマネージ参照を含んでいる場合はバイト配列上にアンマネージ参照が生成される。
+		/// 　バイト配列の使用後はMarshal.DestroyStructure()で参照先のアンマネージメモリを
+		/// 　開放しなければメモリリークする。
+		/// </summary>
+		private static byte[] StructureToBytes(object structure)
+		{
+			int size = Marshal.SizeOf(structure);
+			var buffer = new byte[size];
+
+			var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+			try
+			{
+				var ptr = gch.AddrOfPinnedObject();
+
+				// Marshal.StructureToPtr()がUnmanagedType.ByValTStrに対して行う挙動
+				// ・文字列を固定長に丸める。
+				// ・文字コードをShiftJISコードへ変換する。
+				// ・文字列の(SizeConst-1)位置へヌル文字を書き込む。
+				// ・ヌル文字書き込み位置に全角文字があった場合は例外を発生する。
+				// 　※そのためSizeConstを十分な大きさに設定する必要がある。
+				Marshal.StructureToPtr(structure, ptr, false);
+			}
+			finally
+			{
+				gch.Free();
+			}
+
+			return buffer;
+		}
+
+		/// <summary>
+		/// バイト配列から構造体を生成する
+		/// </summary>
+		private static object BytesToStructure(byte[] buffer, Type structureType)
+		{
+			object structure;
+
+			var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+			try
+			{
+				var ptr = gch.AddrOfPinnedObject();
+				structure = Marshal.PtrToStructure(ptr, structureType);
+			}
+			finally
+			{
+				gch.Free();
+			}
+
+			return structure;
+		}
+
+		/// <summary>
+		/// バイト配列上の構造体からアンマネージ参照を開放する
+		/// </summary>
+		private static void DestroyStructure(byte[] buffer, Type structureType)
+		{
+			var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+			try
+			{
+				var ptr = gch.AddrOfPinnedObject();
+				Marshal.DestroyStructure(ptr, structureType);
+			}
+			finally
+			{
+				gch.Free();
+			}
+		}
+	}
+}
