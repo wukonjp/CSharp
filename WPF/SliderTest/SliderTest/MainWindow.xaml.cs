209ae3312f353df7479055f29515d0f6f5a61be3
wukonjp <wukon@hotmail.com>
2020-12-06 11:14:54 +0900

    追加

---
+++ WPF/SliderTest/SliderTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SliderTest
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			//this.increment.Command = Slider.IncreaseSmall;
+			//this.decrement.Command = new Binding( RoutedCommand("slider" Slider.DecreaseSmall;
+		}
+
+		private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			Console.WriteLine("[{0}] {1}", DateTime.Now.ToString("HH:mm:ss.ff"), e.NewValue);
+		}
+
+		private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			this.DragMove();
+		}
+	}
+}
