977ae830fae34451152a9364bae01e223ead3916
wukonjp <wukon@hotmail.com>
2021-05-15 17:13:44 +0900

    セッション終了の検出

---
+++ WPF/WpfSessionEnd/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace WpfSessionEnd
+{
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application
+	{
+		[STAThread]
+		public static void Main()
+		{
+			var listner =  (DefaultTraceListener)Trace.Listeners["Default"];
+			listner.LogFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+
+			App app = new App();
+			app.InitializeComponent();
+			app.Run();
+		}
+
+		public static void Log(string callerName)
+		{
+			Trace.WriteLine(string.Format("[{0}][{1}]{2}", DateTime.Now, Thread.CurrentThread.ManagedThreadId, callerName));
+		}
+
+		private void Application_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+		{
+			// セッション終了時にのみ呼ばれる
+
+			App.Log("Application_SessionEnding");
+		}
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			App.Log("Application_Startup");
+		}
+
+		private void Application_Exit(object sender, ExitEventArgs e)
+		{
+			App.Log("Application_Exit");
+		}
+	}
+}
---
+++ WPF/WpfSessionEnd/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace WpfSessionEnd
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			App.Log("Window_Constractor");
+		}
+
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			// セッション終了時は呼ばれない
+
+			App.Log("Window_Closed");
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			// セッション終了時は呼ばれない
+
+			MessageBox.Show("終了確認");
+
+			App.Log("Window_Closing");
+		}
+
+		private void Window_Initialized(object sender, EventArgs e)
+		{
+			// ベースクラスのイベント FrameworkElement.Initialized
+
+			App.Log("Window_Initialized");
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			App.Log("Window_Loaded");
+		}
+
+		private void Window_Unloaded(object sender, RoutedEventArgs e)
+		{
+			// 親がいないので呼ばれない
+
+			App.Log("Window_Unloaded");
+		}
+	}
+}
